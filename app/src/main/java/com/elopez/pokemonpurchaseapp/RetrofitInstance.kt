f899acd3e10f4a91d9c843d1a2a4369e68af8b46
elopez <elopez2192@gmail.com>
2023-01-06 15:23:14 -0800

    Created retrofit instance and pokemon api interface

---
+++ app/src/main/java/com/elopez/pokemonpurchaseapp/RetrofitInstance.kt
@@ -0,0 +1,28 @@
+package com.elopez.pokemonpurchaseapp
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object{
+        val BASE_URL = "https://pokeapi.co/"
+
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+    }
+}
